08c4fdc76bb658e2bc7ab3839d7c3e2b7f685e42
Alaba Soneye <soneyeazla@gmail.com>
2024-08-13 23:24:22 +0100

    initial comm

--- next.config.js
+++ next.config.js
@@ -1,5 +1,32 @@
-module.exports = {
-    distDir: 'build',
-    reactStrictMode: true,
-  };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self';",
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Link',
+            value: '</manifest.json>; rel="manifest"',
+          },
+        ],
+      },
+    ];
+  },
   
+  webpack: (config, { isServer }) => {
+    return config;
+  },
+};
+
+module.exports = nextConfig;
